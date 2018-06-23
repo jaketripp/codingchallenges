4567d3bfb4e12720aa6936a01e2d932b38134ee2
Jake <shmogens@gmail.com>
2018-06-23 18:37:54 -0500

    Add problem description to first answer

--- 06-21-2018/Amateur/1.js
+++ 06-21-2018/Amateur/1.js
@@ -1,4 +1,8 @@
 // 1. Longest word
+// Write a function that takes in a sentence and returns the length of the longest word.
+// input: "I like going out to parties with friends or watching TV."
+// output: 8
+
 function findLongestWord(str) {
   return str.split(" ").reduce(function(longest, currentWord) {
     return currentWord.length > longest.length ? currentWord : longest;

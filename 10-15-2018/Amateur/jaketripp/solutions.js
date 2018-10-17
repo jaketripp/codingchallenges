e1c3d5355684b53c20b533e61f565091a75a9e38
Jake Tripp <jake@jake.local>
2018-10-16 20:03:59 -0500

    Update palindrome problem solution to be more optimized

--- 10-15-2018/Amateur/jaketripp/solutions.js
+++ 10-15-2018/Amateur/jaketripp/solutions.js
@@ -91,32 +91,84 @@ console.log(letterMod("$o00jx bnc1mf bg@kkdmfd")); // "$p00ky cOd1ng ch@llEngE"
 // *============================*
 // 5. Largest palindrome product
 // *============================*
+
+// converts the number to a string, then an array, reverses the array, joins the array, and then back into a number
+// 102 -> "102" -> ["1", "0", "2"] -> ["2", "0", "1"] -> "201" -> 201
+// 102 !== 201
 function isPalindrome(num) {
   return parseInt(String(num).split('').reverse().join('')) === num;
 }
-function findLargestPalindromeProduct() {
-  var numberOfDigits = 3;
+
+// by passing as a parameter and calculating the start and end,
+// the function is more adaptable to different use cases
+function findLargestPalindromecurrentProduct(numberOfDigits) {
+
+  // assuming numberOfDigits === 3
+  // start = 100 (10 * 10)
+  // end = 1000 (10 * 10 * 10)
   var start = Math.pow(10, numberOfDigits - 1);
-  var end = Math.pow(10, numberOfDigits);
+  var end = Math.pow(10, numberOfDigits) - 1;
+
   var largestPalindrome = 0;
-  var product;
+  // used to print out the numbers for the largest palindrome product at the end
   var largestPalindromeA = 0;
   var largestPalindromeB = 0;
-  for (a = start; a < end; a++) {
-    for (b = start; b < end; b++) {
-      product = a * b;
-      if (largestPalindrome < product) {
-        if (isPalindrome(product)) {
-          // console.log(`${a} * ${b} = ${product}`);
-          largestPalindrome = product;
+
+  // keep track of the number of iterations
+  // assuming numberOfDigits === 3
+  // iteration count: 42,300
+  // worst possible iteration count: 808,201 
+  // (999 - 100) * (999 * 100)
+  // only 20 times less work, nbd 😉
+  var counter = 0;
+
+  var currentProduct;
+
+  // start in reverse order (less iterations)
+  // Idea courtesy of Maria Vela from the October 2018 Coding Challenge
+  for (a = end; a >= start; a--) {
+    // if the (square root of the largestPalindrome) > a,
+    // we can exit early
+    // assuming numberOfDigits === 3
+    // this is because b changes 899 times for every change of a
+    // because of this, we test a: 999 b:999 all the way through to a:999 b:100
+    // then a:998 b:999 all the way through to a:998 b:100
+    // the largest palindrome is 906609
+    // the sqrt of that is a little greater than 952
+    // because we have tested all the possibilities of b for EVERY a,
+    // when a becomes smaller than the sqrt of the largest palindrome we have found
+    // we can be sure that we can stop checking immediately, 
+    // because a has to be greater than or equal to the sqrt of the largest palindrome
+    // to have a resulting product be greater than or equal to the largest palindrome
+
+    if (Math.sqrt(largestPalindrome) > a) {
+      break;
+    }
+
+    for (b = end; b >= start; b--) {
+
+      counter++;
+      currentProduct = a * b;
+
+      // check if the current product is greater than the largestPalindrome FIRST
+      if (largestPalindrome < currentProduct) {
+
+        // checking if a number is a palindrome is WAY more expensive than comparing the size of two numbers 
+        if (isPalindrome(currentProduct)) {
+
+          console.log(`${a} * ${b} = ${currentProduct}`);
+          largestPalindrome = currentProduct;
           largestPalindromeA = a;
           largestPalindromeB = b;
+
         }
       }
+
     }
   }
 
-  return `${largestPalindromeA} * ${largestPalindromeB} = ${largestPalindrome}`;
-}
-console.log(findLargestPalindromeProduct());
+  return `${largestPalindromeA} * ${largestPalindromeB} = ${largestPalindrome} || ${counter} iterations`;
 
+}
+
+findLargestPalindromecurrentProduct(3)
